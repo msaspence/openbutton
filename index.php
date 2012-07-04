b046888ae09c3434e29338933ef18eeeeeb8e3d0
Mike Carter <mike@mcarter.me>
2012-07-04 10:19:56 +0100

    Added basic display files

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	
+	<link rel="stylesheet" type="text/css" href="main.css" />
+	
+	<title>Fruitworks Coworking Status</title>
+</head>
+<body>
+	<div id="container">
+		<?php include("includes/status_display.php"); ?>
+	</div>
+</body>
+</html>
